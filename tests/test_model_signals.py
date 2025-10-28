b8aa1dd05700ef06775ad6b48dd80a064f3a0cd9
Christian Hartung <hartung@live.com>
2025-10-28 12:23:42 -0300

    fix: task metrics

--- tests/test_model_signals.py
+++ tests/test_model_signals.py
@@ -1,20 +1,14 @@
 import pytest
-from opentelemetry import metrics
-from opentelemetry.sdk.metrics.export import MetricsData
+from opentelemetry.sdk.metrics.export import InMemoryMetricReader, MetricsData
 
 from .models import SimpleModel
-from .utils import create_meter_provider
 
 pytestmark = pytest.mark.django_db
 
 
-def test_model_creation() -> None:
-    meter_provider, memory_metrics_reader = create_meter_provider()
-    metrics.set_meter_provider(meter_provider)
-
+def test_model_creation(metric_reader: InMemoryMetricReader) -> None:
     SimpleModel.objects.create(name="Bob")
-
-    metric_data: MetricsData | None = memory_metrics_reader.get_metrics_data()
+    metric_data: MetricsData | None = metric_reader.get_metrics_data()
 
     assert metric_data is not None
     assert isinstance(metric_data, MetricsData)
@@ -29,22 +23,18 @@ def test_model_creation() -> None:
                     assert data_points[0].attributes["model"] == "simple_model"  # type: ignore[attr-defined]
                     return
 
-                if metric.name in ["django_model_updates", "django_model_deletes"]:
-                    pytest.fail(f"Should not get {metric.name} metric")
+                if metric.name.startswith("django_model"):
+                    pytest.fail(f"Unexpected metric {metric.name}")
 
     pytest.fail("Metric not found")
 
 
-def test_model_update() -> None:
+def test_model_update(metric_reader: InMemoryMetricReader) -> None:
     instance = SimpleModel.objects.create(name="Bob")
-
-    meter_provider, memory_metrics_reader = create_meter_provider()
-    metrics.set_meter_provider(meter_provider)
 
     instance.name = "Mark"
     instance.save()
-
-    metric_data: MetricsData | None = memory_metrics_reader.get_metrics_data()
+    metric_data: MetricsData | None = metric_reader.get_metrics_data()
 
     assert metric_data is not None
     assert len(metric_data.resource_metrics) > 0
@@ -58,21 +48,14 @@ def test_model_update() -> None:
                     assert data_points[0].attributes["model"] == "simple_model"  # type: ignore[attr-defined]
                     return
 
-                if metric.name in ["django_model_inserts", "django_model_deletes"]:
-                    pytest.fail(f"Should not get {metric.name} metric")
-
     pytest.fail("Metric not found")
 
 
-def test_model_delete() -> None:
+def test_model_delete(metric_reader: InMemoryMetricReader) -> None:
     instance = SimpleModel.objects.create(name="Bob")
 
-    meter_provider, memory_metrics_reader = create_meter_provider()
-    metrics.set_meter_provider(meter_provider)
-
     instance.delete()
-
-    metric_data: MetricsData | None = memory_metrics_reader.get_metrics_data()
+    metric_data: MetricsData | None = metric_reader.get_metrics_data()
 
     assert metric_data is not None
     assert len(metric_data.resource_metrics) > 0
@@ -85,8 +68,5 @@ def test_model_delete() -> None:
                     assert len(data_points) == 1
                     assert data_points[0].attributes["model"] == "simple_model"  # type: ignore[attr-defined]
                     return
-
-                if metric.name in ["django_model_inserts", "django_model_updates"]:
-                    pytest.fail(f"Should not get {metric.name} metric")
 
     pytest.fail("Metric not found")
