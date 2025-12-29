ccc658527ef3df70a9ae15e920ea49376b24bb6e
Christian Hartung <6785871+hartungstenio@users.noreply.github.com>
2025-12-29 14:23:18 -0300

    build: use hatch-vcs to set package version

--- src/django_opentelemetry/__about__.py
+++
@@ -1,4 +0,0 @@
-# SPDX-FileCopyrightText: 2025-present Christian Hartung <christian.hartung@olist.com>
-#
-# SPDX-License-Identifier: MIT
-__version__ = "0.0.1"
--- tests/__init__.py
+++
@@ -1,3 +0,0 @@
-# SPDX-FileCopyrightText: 2025-present Christian Hartung <christian.hartung@olist.com>
-#
-# SPDX-License-Identifier: MIT
